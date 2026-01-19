d0d993af1d10fb6042bc2e04dd4669c6be7e5cbb
Eduardo <eduardoribeirodefraga05@gmail.com>
2026-01-19 09:13:30 -0300

    feat: implementa componente ListaTemas e corrige rotas no App

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import Home from "./pages/home/Home"
 import Cadastro from './pages/cadastro/Cadastro'
 import Login from './pages/login/Login'
 import { AuthProvider } from './contexts/AuthContext'
-import CardTema from './components/tema/cardtema/CardTema'
-CardTema
+import ListaTemas from './components/tema/cardtema/listatemas/ListaTemas'
+
 
 function App() {
 
@@ -20,7 +20,7 @@ function App() {
             <Route path="/" element={<Login />}></Route>
             <Route path="/home" element={<Home />}></Route>
             <Route path="/cadastro" element={<Cadastro />}></Route>
-            <Route path="/temas" element={<CardTema />}></Route>
+            <Route path="/temas" element={<ListaTemas />}></Route>
           </Routes>
         </div>
         <Footer />
