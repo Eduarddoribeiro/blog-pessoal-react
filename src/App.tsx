33019ac7a656953063e7e0853767747478c9c1f4
Eduardo <eduardoribeirodefraga05@gmail.com>
2026-01-19 09:07:32 -0300

    feat: cria componente CardTema e configura rotas

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Home from "./pages/home/Home"
 import Cadastro from './pages/cadastro/Cadastro'
 import Login from './pages/login/Login'
 import { AuthProvider } from './contexts/AuthContext'
+import CardTema from './components/tema/cardtema/CardTema'
+CardTema
 
 function App() {
 
@@ -18,6 +20,7 @@ function App() {
             <Route path="/" element={<Login />}></Route>
             <Route path="/home" element={<Home />}></Route>
             <Route path="/cadastro" element={<Cadastro />}></Route>
+            <Route path="/temas" element={<CardTema />}></Route>
           </Routes>
         </div>
         <Footer />
