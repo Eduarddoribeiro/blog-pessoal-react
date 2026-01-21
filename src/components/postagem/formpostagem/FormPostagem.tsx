1510a3feded532cb356649770c82298e13c8b864
Eduardo <eduardoribeirodefraga05@gmail.com>
2026-01-21 10:03:25 -0300

    feat(postagens): integra lógica de cadastro e atualização no FormPostagem

--- src/components/postagem/formpostagem/FormPostagem.tsx
+++ src/components/postagem/formpostagem/FormPostagem.tsx
@@ -1,49 +1,200 @@
+import { type ChangeEvent, type FormEvent, useContext, useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { cadastrar, atualizar, buscar } from '../../../services/Service';
+import { AuthContext } from '../../../contexts/AuthContext';
+import type Postagem from '../../../models/Postagem';
+import type Tema from '../../../models/Tema';
+import { ClipLoader } from 'react-spinners';
+
 function FormPostagem() {
-  return (
-    <div className="container flex flex-col mx-auto items-center">
-      <h1 className="text-4xl text-center my-8">Cadastrar Postagem</h1>
 
-      <form className="flex flex-col w-1/2 gap-4">
-        <div className="flex flex-col gap-2">
-          <label htmlFor="titulo">Título da Postagem</label>
-          <input
-            type="text"
-            placeholder="Titulo"
-            name="titulo"
-            required
-            className="border-2 border-slate-700 rounded p-2"
-          />
-        </div>
-        <div className="flex flex-col gap-2">
-          <label htmlFor="titulo">Texto da Postagem</label>
-          <input
-            type="text"
-            placeholder="Texto"
-            name="texto"
-            required
-            className="border-2 border-slate-700 rounded p-2"
-          />
-        </div>
-        <div className="flex flex-col gap-2">
-          <p>Tema da Postagem</p>
-          <select name="tema" id="tema" className='border p-2 border-slate-800 rounded'>
-            <option value="" selected disabled>Selecione um Tema</option>
+    const navigate = useNavigate();
 
-            <>
-                <option>tema1</option>
-            </>
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [temas, setTemas] = useState<Tema[]>([]);
 
-          </select>
+    const [tema, setTema] = useState<Tema>({ id: 0, descricao: '', });
+    const [postagem, setPostagem] = useState<Postagem>({} as Postagem);
+
+    const { id } = useParams<{ id: string }>();
+
+    const { usuario, handleLogout } = useContext(AuthContext);
+    const token = usuario.token;
+
+    async function buscarPostagemPorId(id: string) {
+        try {
+            await buscar(`/postagens/${id}`, setPostagem, {
+                headers: { Authorization: token }
+            })
+        } catch (error: any) {
+            if (error.toString().includes('403')) {
+                handleLogout()
+            }
+        }
+    }
+
+    async function buscarTemaPorId(id: string) {
+        try {
+            await buscar(`/temas/${id}`, setTema, {
+                headers: { Authorization: token }
+            })
+        } catch (error: any) {
+            if (error.toString().includes('403')) {
+                handleLogout()
+            }
+        }
+    }
+
+    async function buscarTemas() {
+        try {
+            await buscar('/temas', setTemas, {
+                headers: { Authorization: token }
+            })
+        } catch (error: any) {
+            if (error.toString().includes('403')) {
+                handleLogout()
+            }
+        }
+    }
+
+    useEffect(() => {
+        if (token === '') {
+            alert('Você precisa estar logado');
+            navigate('/');
+        }
+    }, [token])
+
+    useEffect(() => {
+        buscarTemas();
+        if (id !== undefined) {
+            buscarPostagemPorId(id);
+        }
+    }, [id])
+
+    useEffect(() => {
+        setPostagem({
+            ...postagem,
+            tema: tema,
+        })
+    }, [tema])
+
+    function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+        setPostagem({
+            ...postagem,
+            [e.target.name]: e.target.value,
+            tema: tema,
+            usuario: usuario,
+        });
+    }
+
+    function retornar() {
+        navigate('/postagens');
+    }
+
+    async function gerarNovaPostagem(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault();
+        setIsLoading(true);
+
+        if (id !== undefined) {
+            try {
+                await atualizar(`/postagens`, postagem, setPostagem, {
+                    headers: { Authorization: token },
+                });
+                alert('Postagem atualizada com sucesso');
+                retornar();
+            } catch (error: any) {
+                if (error.toString().includes('403')) {
+                    handleLogout();
+                } else {
+                    alert('Erro ao atualizar a Postagem');
+                }
+            }
+        } else {
+            try {
+                await cadastrar(`/postagens`, postagem, setPostagem, {
+                    headers: { Authorization: token },
+                });
+                alert('Postagem cadastrada com sucesso');
+                retornar();
+            } catch (error: any) {
+                if (error.toString().includes('403')) {
+                    handleLogout();
+                } else {
+                    alert('Erro ao cadastrar a Postagem');
+                }
+            }
+        }
+
+        setIsLoading(false);
+    }
+
+    const carregandoTema = tema.descricao === '';
+
+    return (
+        <div className="container flex flex-col mx-auto items-center">
+            <h1 className="text-4xl text-center my-8">{id !== undefined
+                ? 'Editar Postagem'
+                : 'Cadastrar Postagem'
+            }</h1>
+
+            <form className="flex flex-col w-1/2 gap-4"
+                onSubmit={gerarNovaPostagem}>
+                <div className="flex flex-col gap-2">
+                    <label htmlFor="titulo">Título da Postagem</label>
+                    <input
+                        type="text"
+                        placeholder="Titulo"
+                        name="titulo"
+                        required
+                        className="border-2 border-slate-700 rounded p-2"
+                        value={postagem.titulo}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+                    />
+                </div>
+                <div className="flex flex-col gap-2">
+                    <label htmlFor="titulo">Texto da Postagem</label>
+                    <input
+                        type="text"
+                        placeholder="Texto"
+                        name="texto"
+                        required
+                        className="border-2 border-slate-700 rounded p-2"
+                        value={postagem.texto}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+                    />
+                </div>
+                <div className="flex flex-col gap-2">
+                    <p>Tema da Postagem</p>
+                    <select name="tema" id="tema" className='border p-2 border-slate-800 rounded'
+                        onChange={async (e) => await buscarTemaPorId(e.target.value)}>
+                        <option value="" selected disabled>Selecione um Tema</option>
+
+                        {temas.map((tema) => (
+                            <>
+                            <option value={tema.id} > {tema.descricao}
+                            
+                            </option>
+                            </>
+                        ))}
+
+                    </select>
+                </div>
+                <button
+                    type='submit'
+                    className='rounded disabled:bg-slate-200 bg-indigo-400 hover:bg-indigo-800 text-white font-bold w-1/2 mx-auto py-2 flex justify-center'
+                    disabled={carregandoTema}
+                >
+                    { isLoading ? 
+                        <ClipLoader
+                            color="#ffffff"
+                            size={24}
+                            /> :
+                            <span>{id === undefined ? 'Cadastrar' : 'Atualizar'}</span>
+                    }
+                </button>
+            </form>
         </div>
-        <button
-            type='submit'
-            className='rounded disabled:bg-slate-200 bg-indigo-400 hover:bg-indigo-800 text-white font-bold w-1/2 mx-auto py-2 flex justify-center'
-        >
-            Cadastrar
-        </button>
-      </form>
-    </div>
-  );
+    );
 }
 
 export default FormPostagem;
